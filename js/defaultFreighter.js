49df06bc2e634da7ec7a2a547005539640087918
Kamoteshake <kamoteshake@gmail.com>
2022-05-19 00:24:06 -0500

    updated default freighter layout

--- js/defaultFreighter.js
+++ js/defaultFreighter.js
@@ -1,58 +1,69 @@
-const buildDefaultFreighter = (grid) => {
-  grid[0][10].updateTile(LARGE_ROOM, 0, true);
+const buildDefaultFreighter = () => {
+  const grid = make2dArray(MAX_COLUMN, MAX_ROW)
+  // create empty grid
+  for(let x = 0; x < MAX_COLUMN; x++) {
+    for(let y = 0; y < MAX_ROW; y++) {
+      const newTile = new Tile(x, y, 0, EMPTY, false);
+      grid[x][y] = newTile;
+    }
+  }
 
-  grid[1][7].updateTile(LARGE_ROOM);
-  grid[1][8].updateTile(LARGE_ROOM);
-  grid[1][9].updateTile(LARGE_ROOM);
-  grid[1][10].updateTile(LARGE_ROOM, 0, true);
-  grid[1][11].updateTile(LARGE_ROOM);
-  grid[1][12].updateTile(LARGE_ROOM);
+  grid[10][0].updateTile(LARGE_ROOM, 0, true);
 
-  grid[2][9].updateTile(LARGE_ROOM);
-  grid[2][10].updateTile(STAIRS_DOWN, 0, true);
-  grid[2][12].updateTile(LARGE_ROOM);
+  grid[7][1].updateTile(LARGE_ROOM);
+  grid[8][1].updateTile(LARGE_ROOM);
+  grid[9][1].updateTile(LARGE_ROOM);
+  grid[10][1].updateTile(LARGE_ROOM, 0, true);
+  grid[11][1].updateTile(LARGE_ROOM);
+  grid[12][1].updateTile(LARGE_ROOM);
 
-  grid[3][9].updateTile(LARGE_ROOM);
-  grid[3][12].updateTile(LARGE_ROOM);
+  grid[9][2].updateTile(LARGE_ROOM);
+  grid[10][2].updateTile(STAIRS_DOWN, 0, true);
+  grid[12][2].updateTile(LARGE_ROOM);
 
-  grid[4][7].updateTile(LARGE_ROOM);
-  grid[4][8].updateTile(LARGE_ROOM);
-  grid[4][9].updateTile(LARGE_ROOM);
-  grid[4][10].updateTile(LARGE_ROOM);
-  grid[4][11].updateTile(LARGE_ROOM);
-  grid[4][12].updateTile(LARGE_ROOM);
+  grid[9][3].updateTile(LARGE_ROOM);
+  grid[12][3].updateTile(LARGE_ROOM);
 
-  grid[5][9].updateTile(CORRIDOR);
-  grid[5][11].updateTile(CORRIDOR);
+  grid[7][4].updateTile(LARGE_ROOM);
+  grid[8][4].updateTile(LARGE_ROOM);
+  grid[9][4].updateTile(LARGE_ROOM);
+  grid[10][4].updateTile(LARGE_ROOM);
+  grid[11][4].updateTile(LARGE_ROOM);
+  grid[12][4].updateTile(LARGE_ROOM);
 
-  grid[6][9].updateTile(CORRIDOR);
-  grid[6][11].updateTile(CORRIDOR);
+  grid[9][5].updateTile(CORRIDOR);
+  grid[11][5].updateTile(CORRIDOR);
 
-  grid[7][9].updateTile(LARGE_ROOM);
-  grid[7][10].updateTile(LARGE_ROOM);
-  grid[7][11].updateTile(LARGE_ROOM);
+  grid[9][6].updateTile(CORRIDOR);
+  grid[11][6].updateTile(CORRIDOR);
+
+  grid[9][7].updateTile(LARGE_ROOM);
+  grid[10][7].updateTile(LARGE_ROOM);
+  grid[11][7].updateTile(LARGE_ROOM);
 
   grid[8][8].updateTile(LARGE_ROOM);
-  grid[8][9].updateTile(LARGE_ROOM);
-  grid[8][10].updateTile(LARGE_ROOM);
-  grid[8][11].updateTile(LARGE_ROOM);
-  grid[8][12].updateTile(LARGE_ROOM);
-
   grid[9][8].updateTile(LARGE_ROOM);
-  grid[9][9].updateTile(LARGE_ROOM);
-  grid[9][10].updateTile(LARGE_ROOM);
-  grid[9][11].updateTile(LARGE_ROOM);
-  grid[9][12].updateTile(LARGE_ROOM);
-
   grid[10][8].updateTile(LARGE_ROOM);
-  grid[10][9].updateTile(LARGE_ROOM);
-  grid[10][10].updateTile(LARGE_ROOM);
-  grid[10][11].updateTile(LARGE_ROOM);
-  grid[10][12].updateTile(LARGE_ROOM);
-
   grid[11][8].updateTile(LARGE_ROOM);
+  grid[12][8].updateTile(LARGE_ROOM);
+
+  grid[8][9].updateTile(LARGE_ROOM);
+  grid[9][9].updateTile(LARGE_ROOM);
+  grid[10][9].updateTile(LARGE_ROOM);
   grid[11][9].updateTile(LARGE_ROOM);
+  grid[12][9].updateTile(LARGE_ROOM);
+
+  grid[8][10].updateTile(LARGE_ROOM);
+  grid[9][10].updateTile(LARGE_ROOM);
+  grid[10][10].updateTile(LARGE_ROOM);
   grid[11][10].updateTile(LARGE_ROOM);
+  grid[12][10].updateTile(LARGE_ROOM);
+
+  grid[8][11].updateTile(LARGE_ROOM);
+  grid[9][11].updateTile(LARGE_ROOM);
+  grid[10][11].updateTile(LARGE_ROOM);
   grid[11][11].updateTile(LARGE_ROOM);
-  grid[11][12].updateTile(LARGE_ROOM);
-}
+  grid[12][11].updateTile(LARGE_ROOM);
+
+  return grid;
+};
