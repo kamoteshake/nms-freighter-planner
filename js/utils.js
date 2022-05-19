bf40f0f7f3b621ecf8c6ed36362fa760063db23a
Kamoteshake <kamoteshake@gmail.com>
2022-05-19 02:18:50 -0500

    added coords from mouse pos util

--- js/utils.js
+++ js/utils.js
@@ -6,3 +6,18 @@ const make2dArray = (columns, rows) => {
 
   return newArray;
 };
+
+// get grid coordinates from mouse position
+const getGridCoorsFromMousePos = mouseEvent => {
+  const rect = mouseEvent.target.getBoundingClientRect();
+
+  let x = Math.floor((mouseEvent.clientX - rect.left) / TILE_SIZE);
+  let y = Math.floor((mouseEvent.clientY - rect.top) / TILE_SIZE);
+
+  if (x < 0) x = 0;
+  if (y < 0) y = 0;
+  if (x >= MAX_COLUMN) x = MAX_COLUMN - 1;
+  if (y >= MAX_ROW) y = MAX_ROW - 1;
+
+  return { x, y };
+};
