2a925f23c030e6e902dc0edc31890d5287807932
Kamoteshake <kamoteshake@gmail.com>
2022-05-18 19:18:38 -0500

    updated constants and grid array

--- js/setup.js
+++ js/setup.js
@@ -1,11 +1,12 @@
 // config
-const maxRow = 21;
-const maxColumn = 21;
-const maxFloor = 14;
-const maxStorageUnit = 10;
-const maxOrbitalExocraftMaterializer = 1;
+const TILE_SIZE = 50;
+const MAX_ROW = 21;
+const MAX_COLUMN = 21;
+const MAX_FLOOR = 14;
+const MAX_STORAGE_UNIT = 10;
+const MAX_ORBITAL_EXOCRAFT_MATERIALIZER = 1;
 
-let grid = [];
+let grid = make2dArray(MAX_COLUMN, MAX_ROW);
 let storageUnitCount = parseInt(localStorage.getItem('storageUnitCount') ?? 0)
 let orbitalExocraftMaterializerCount = parseInt(localStorage.getItem('orbitalExocraftMaterializerCount') ?? 0);
 let currentFloor = parseInt(localStorage.getItem('currentFloor') ?? 0);
@@ -88,7 +89,7 @@ const handleTableClick = e => {
 
     // add storage unit count
     if (currentType === STORAGE_UNIT) {
-      if (storageUnitCount === maxStorageUnit) return;
+      if (storageUnitCount === MAX_STORAGE_UNIT) return;
 
       storageUnitCount += 1;
       localStorage.setItem('storageUnitCount', storageUnitCount);
@@ -106,7 +107,7 @@ const handleTableClick = e => {
 
     // add orbital exocraft materializer count
     if (currentType === ORBITAL_EXOCRAFT_MATERIALIZER) {
-      if (orbitalExocraftMaterializerCount === maxOrbitalExocraftMaterializer) return;
+      if (orbitalExocraftMaterializerCount === MAX_ORBITAL_EXOCRAFT_MATERIALIZER) return;
 
       orbitalExocraftMaterializerCount += 1;
       localStorage.setItem('orbitalExocraftMaterializerCount', orbitalExocraftMaterializerCount);
@@ -135,11 +136,11 @@ const populateTableGrid = () => {
 
   // create new empty grid if there are no grid
   if (!grid.length) {
-    for(let row = 0; row < maxRow; row++) {
+    for(let row = 0; row < MAX_ROW; row++) {
       const rowArray = [];
   
       // create column
-      for(let column = 0; column < maxColumn; column++) {
+      for(let column = 0; column < MAX_COLUMN; column++) {
         const newTile = new Tile(row, column);
         rowArray.push(newTile);
   
@@ -179,7 +180,7 @@ const updateFloorButtons = () => {
   if (currentFloor === 0) {
     previousFloorButton.disabled = true;
   }
-  else if (currentFloor === maxFloor - 1) {
+  else if (currentFloor === MAX_FLOOR - 1) {
     nextFloorButton.disabled = true;
   }
   else {
@@ -195,27 +196,27 @@ const updateFloorButtons = () => {
 const updateStorageUnitButton = () => {
   const storageUnitButton = document.getElementById('storageUnitButton');
 
-  if (storageUnitCount === maxStorageUnit && !storageUnitButton.disabled) {
+  if (storageUnitCount === MAX_STORAGE_UNIT && !storageUnitButton.disabled) {
     storageUnitButton.disabled = true;
   }
-  if (storageUnitCount !== maxStorageUnit && storageUnitButton.disabled) {
+  if (storageUnitCount !== MAX_STORAGE_UNIT && storageUnitButton.disabled) {
     storageUnitButton.disabled = false;
   }
 
-  storageUnitButton.getElementsByTagName('span')[0].innerText = `Storage Unit (${maxStorageUnit - storageUnitCount})`;
+  storageUnitButton.getElementsByTagName('span')[0].innerText = `Storage Unit (${MAX_STORAGE_UNIT - storageUnitCount})`;
 };
 
 const updateOrbitalExocraftMaterializerButton = () => {
   const orbitalExocraftMaterializerButton = document.getElementById('orbitalExocraftMaterializerButton');
 
-  if (orbitalExocraftMaterializerCount === maxOrbitalExocraftMaterializer && !orbitalExocraftMaterializerButton.disabled) {
+  if (orbitalExocraftMaterializerCount === MAX_ORBITAL_EXOCRAFT_MATERIALIZER && !orbitalExocraftMaterializerButton.disabled) {
     orbitalExocraftMaterializerButton.disabled = true;
   }
-  if (orbitalExocraftMaterializerCount !== maxOrbitalExocraftMaterializer && orbitalExocraftMaterializerButton.disabled) {
+  if (orbitalExocraftMaterializerCount !== MAX_ORBITAL_EXOCRAFT_MATERIALIZER && orbitalExocraftMaterializerButton.disabled) {
     orbitalExocraftMaterializerButton.disabled = false;
   }
 
-  orbitalExocraftMaterializerButton.getElementsByTagName('span')[0].innerText = `Orbital Exocraft Materializer (${maxOrbitalExocraftMaterializer - orbitalExocraftMaterializerCount})`;
+  orbitalExocraftMaterializerButton.getElementsByTagName('span')[0].innerText = `Orbital Exocraft Materializer (${MAX_ORBITAL_EXOCRAFT_MATERIALIZER - orbitalExocraftMaterializerCount})`;
 };
 
 const updateTilePreview = () => {
@@ -228,39 +229,68 @@ const renderContent = () => {
   populateTableGrid();
 }
 
-const initiate = () => {
-  // build empty grid
-  const gridTable = document.getElementById('gridTable');
-  const tableBody = document.createElement('tbody');
+let lastHovered = { x: null, y: null };
 
-  tableBody.addEventListener('mousedown', handleTableClick);
-  tableBody.addEventListener('mouseover', handleTableClick);
+const mouseEvent = (e) => {
+  const rect = e.target.getBoundingClientRect();
 
+  let x = Math.floor((e.clientX - rect.left) / TILE_SIZE);
+  let y = Math.floor((e.clientY - rect.top) / TILE_SIZE);
 
-  // create row
-  for(let row = 0; row < maxRow; row++) {
-    const tableRow = document.createElement('tr');
+  if (x < 0) x = 0;
+  if (y < 0) y = 0;
+  if (x >= MAX_COLUMN) x = MAX_COLUMN - 1;
+  if (y >= MAX_ROW) y = MAX_ROW - 1;
 
-    // create column
-    for(let column = 0; column < maxColumn; column++) {
-      const tableColumn = document.createElement('td');
-      tableColumn.id = `tile_${row}_${column}`;
+  if (lastHovered.x === x && lastHovered.y === y) return;
 
-      tableRow.appendChild(tableColumn);
-    }
-
-    tableBody.appendChild(tableRow);
+  if (lastHovered.x !== null && lastHovered.y !== null) {
+    const lastHoveredTile = grid[lastHovered.x][lastHovered.y];
+    lastHoveredTile.mouseExit();
+    lastHoveredTile.draw();
   }
 
-  gridTable.appendChild(tableBody);
+  const hoveredTile = grid[x][y];
+  hoveredTile.mouseEnter();
+  hoveredTile.draw();
+
+  lastHovered = { x, y };
+};
+
+const initiate = () => {
+  // setup canvas
+  const canvas = document.getElementById('canvas');
+  canvas.setAttribute('width', TILE_SIZE * MAX_COLUMN);
+  canvas.setAttribute('height', TILE_SIZE * MAX_ROW);
+
+  canvas.addEventListener('mousemove', mouseEvent);
+
+  // create empty grid
+  for(let x = 0; x < MAX_COLUMN; x++) {
+    for(let y = 0; y < MAX_ROW; y++) {
+      const newTile = new EmptyRoom(x, y, 0, false);
+      grid[x][y] = newTile;
+    }
+  }
+
+  draw();
 
   // draw tile preview
-  const tilePreviewElement = document.getElementById('tilePreview');
-  tilePreviewElement.appendChild(tilePreview.draw());
+  // const tilePreviewElement = document.getElementById('tilePreview');
+  // tilePreviewElement.appendChild(tilePreview.draw());
 
-  renderContent();
-  updateStorageUnitButton();
-  updateOrbitalExocraftMaterializerButton();
+  // renderContent();
+  // updateStorageUnitButton();
+  // updateOrbitalExocraftMaterializerButton();
+};
+
+const draw = () => {
+  // draw each tiles
+  grid.forEach(tiles => {
+    tiles.forEach(tile => {
+      tile.draw();
+    })
+  })
 };
 
 document.addEventListener('DOMContentLoaded', initiate);
