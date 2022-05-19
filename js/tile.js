68f6230f4d70f23ed37f3b5e23fa50e4f4cb91a8
Kamoteshake <kamoteshake@gmail.com>
2022-05-19 02:19:36 -0500

    added rotation to tile

--- js/tile.js
+++ js/tile.js
@@ -21,19 +21,31 @@ class Tile {
     this.rotation = rotation;
   }
 
-  updateTile(type, rotation, isFixed) {
+  updateIsFixed(isFixed) {
+    this.isFixed = isFixed;
+  }
+
+  updateTile(type, rotation) {
+    // only update the tile if it's not fixed
+    if (this.isFixed) return;
+
     this.type = type;
     this.img = typeImages[type];
     this.rotation = rotation;
-    this.isFixed = isFixed;
   }
 
   draw() {
     const ctx = document.getElementById('canvas').getContext('2d');
     const img = new Image();
     img.onload = () => {
+      ctx.save();
+      const translateX = (this.x * TILE_SIZE) + TILE_SIZE * 0.5;
+      const translateY = (this.y * TILE_SIZE) + TILE_SIZE * 0.5;
+
+      ctx.translate(translateX, translateY);
       ctx.rotate((Math.PI / 180) * this.rotation);
-      ctx.drawImage(img, this.x * TILE_SIZE, this.y * TILE_SIZE);
+      ctx.drawImage(img, -(TILE_SIZE * 0.5), -(TILE_SIZE * 0.5));
+      ctx.restore();
 
       // add a green border when hovered and it's not a fixed tile
       if (this.hovered && !this.isFixed) {
